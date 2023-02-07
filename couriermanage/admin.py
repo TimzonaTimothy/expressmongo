450e7762942e980b274b709feeaa545e7cd31a06
Arinze Timothy <78275394+TimzonaTimothy@users.noreply.github.com>
2023-02-07 13:22:16 +0100

    email

--- couriermanage/admin.py
+++ couriermanage/admin.py
@@ -3,9 +3,12 @@ from .models import *
 
 
 class CourierAdmin(admin.ModelAdmin):
-	list_display = ('tracking_id', 'service_type','content',)
+	list_display = ('tracking_id','sender_name', 'service_type','content',)
+	list_display_links = ('tracking_id','sender_name', 'service_type','content',)
 	# change_list_template = 'admin/change_list.html'
 	actions = None
+	readonly_fields = ('sent',)
+	list_per_page = 25
 
 class TransactionAdmin(admin.ModelAdmin):
 	list_display = ( 'parcel', 'details', 'activity','date')
