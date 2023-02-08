03f83e6186c19b7d8313e2316b4a5cdfbd4a332c
Arinze Timothy <78275394+TimzonaTimothy@users.noreply.github.com>
2023-02-08 18:54:20 +0100

    done

--- couriermanage/createsu.py
+++ couriermanage/createsu.py
@@ -7,9 +7,9 @@ class Command(BaseCommand):
     help = 'Creates a superuser.'
 
     def handle(self, *args, **options):
-        if not User.objects.filter(username='admin').exists():
+        if not User.objects.filter(username='admin1').exists():
             User.objects.create_superuser(
-                username='admin',
+                username='admin1',
                 password='express1111'
             )
         print('Superuser has been created.')
